42d81257236161ab4e72f0085478a4cb83b9464e
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-01 17:21:15 +0530

    Add axios for API calls, useEffect for calling main function on load
    Adds axios library for making HTTP requests, and uses the useEffect hook to call the main function when the component mounts. The main function makes a GET request to a specific endpoint and returns the response data or an error object.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
+
 
 function App() {
+  useEffect(() => {
+    main();
+  }, []);
+
+  function main() {
+    return axios
+      .get(`https://device-probe.vercel.app/`)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        return { res: error, status: false, statusCode: 500, error: "error" };
+      });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
