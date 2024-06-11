c1879d1330772b994041a3560e385ec51458a50a
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-11 13:20:26 +0530

    Remove axios import from trap.js

--- src/trap.js
+++ src/trap.js
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
-import axios from "axios";
 import { useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
