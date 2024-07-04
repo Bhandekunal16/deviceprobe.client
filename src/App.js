d330a15b4a19be02c2def0cfb63522bb320f6809
bhandekunal16 <bhandekunal16@gmail.com>
2024-07-04 16:56:11 +0530

    "Moved router and routes from App.js to new Layout.js component"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import "./style/App.css";
-import Trap from "./trap";
-import Dashboard from "./dashboard";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route as RouteV6,
-} from "react-router-dom";
-import SpeedTest from "./SpeedTest";
+import Layout from "./layout";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <RouteV6 path="" element={<Trap />} />
-          <RouteV6 path="Dashboard" element={<Dashboard />} />
-          <RouteV6 path="speed" element={<SpeedTest />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Layout />
+    </>
   );
 }
 
