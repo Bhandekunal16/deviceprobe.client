2417cd916418e8e891bb5757aa7bdbe1a0484576
bhandekunal16 <bhandekunal16@gmail.com>
2024-07-04 17:28:10 +0530

    "Moved router configuration from Layout component to App component and removed Layout component"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./style/App.css";
-import Layout from "./layout";
+import React from "react";
+import Trap from "./trap";
+import Dashboard from "./dashboard";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route as RouteV6,
+} from "react-router-dom";
+import SpeedTest from "./SpeedTest";
 
 function App() {
   return (
     <>
-      <Layout />
+      <Router>
+        <div>
+          <Routes>
+            <RouteV6 path="" element={<Trap />} />
+            <RouteV6 path="Dashboard" element={<Dashboard />} />
+            <RouteV6 path="speed" element={<SpeedTest />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
